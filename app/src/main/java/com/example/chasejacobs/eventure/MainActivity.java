4cc256c1b0bc510e83f0ab5ee9b5ee0136a19b1f
Luke <luke.iannucci@gmail.com>
2016-06-28 02:23:14 -0600

    added a cool dialog to make sure the user is connected to the internet on the main page

--- app/src/main/java/com/example/chasejacobs/eventure/MainActivity.java
+++ app/src/main/java/com/example/chasejacobs/eventure/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.chasejacobs.eventure;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,25 +21,55 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<events> yourEvents;
     Firebase mRef;
     TextView newText;
     private static final String errorMsg = "MainActivity";
+    ConnectivityManager connMgr;
+    NetworkInfo networkInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = connMgr.getActiveNetworkInfo();
         yourEvents = new ArrayList<events>();
+        if (networkInfo == null) {
+            createNetErrorDialog();
+        }
     }
 
-    protected void onStart(){
+    protected void createNetErrorDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You need a network connection to use this application. Please turn on mobile network or Wi-Fi in Settings.")
+                .setTitle("Unable to connect")
+                .setCancelable(false)
+                .setPositiveButton("Settings",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                                startActivity(i);
+                            }
+                        }
+                )
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                MainActivity.this.finish();
+                            }
+                        }
+                );
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    protected void onStart() {
         super.onStart();
-        newText = (TextView)findViewById(R.id.newTest);
+        newText = (TextView) findViewById(R.id.newTest);
         mRef = new Firebase("https://eventure-860cb.firebaseio.com/testing");
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -44,43 +80,58 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-
             }
         });
     }
 
 
-    public void onButtonClick(View a){
-        if (a.getId() == R.id.testNext){
-            Intent i = new Intent (this, createPage.class);
-            startActivity(i);
-        }
-    }
-
-    public void onButtonClickTest(View a){
-        if(a.getId() == R.id.eventInfo){
-            try{
-                Intent i = new Intent (this, EventInfo.class);
+    public void onButtonClick(View a) {
+        if (a.getId() == R.id.testNext) {
+            connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo == null) {
+                createNetErrorDialog();
+            } else {
+                Intent i = new Intent(this, createPage.class);
                 startActivity(i);
             }
-            catch (Exception o) {
-                Log.e(errorMsg, o.toString());
+        }
+    }
+
+    public void onButtonClickTest(View a) {
+        if (a.getId() == R.id.eventInfo) {
+            connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo == null) {
+                createNetErrorDialog();
+            } else {
+                try {
+                    Intent i = new Intent(this, EventInfo.class);
+                    startActivity(i);
+                } catch (Exception o) {
+                    Log.e(errorMsg, o.toString());
+                }
             }
         }
     }
 
-    public void searchPage(View a){
-        if (a.getId() == R.id.searchEvents){
-            Intent i = new Intent(this, SearchPage.class);
-            startActivity(i);
+    public void searchPage(View a) {
+        if (a.getId() == R.id.searchEvents) {
+            if (networkInfo == null) {
+                connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                networkInfo = connMgr.getActiveNetworkInfo();
+                createNetErrorDialog();
+            } else {
+                Intent i = new Intent(this, SearchPage.class);
+                startActivity(i);
+            }
         }
     }
 
-    public void unitTestReceiveGames(View a){
+    public void unitTestReceiveGames(View a) {
         String eventName = "Soccer";
         String creatorName = "Chase Jacobs";
         String location = "BYUI Lower fields";
-        //Date dateObject;
         int NumPeopleGoing = 4;
         int peopleLimit = 24;
         ArrayList<String> peopleGoing = new ArrayList<String>();
@@ -101,23 +152,28 @@ public class MainActivity extends AppCompatActivity {
         yourEvents.add(event);
     }
 
-    public void unitTestAddPerson(events event){
+    public void unitTestAddPerson(events event) {
         event.addPersonGoing("Joe");
         int numPeople = event.getNumPeopleGoing();
         numPeople++;
         event.setNumPeopleGoing(numPeople);
     }
 
-    public void unitTestDatabase(View a){
-        if (a.getId() == R.id.unitTestDatabase){
-            mRef = new Firebase("https://eventure-860cb.firebaseio.com//testing");
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("testing");
+    public void unitTestDatabase(View a) {
+        if (a.getId() == R.id.unitTestDatabase) {
+            connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo == null) {
+                createNetErrorDialog();
+            } else {
+                mRef = new Firebase("https://eventure-860cb.firebaseio.com//testing");
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("testing");
+                newText = (TextView) findViewById(R.id.newTest);
+                String text = newText.getText().toString();
+                myRef.setValue(text);
+            }
 
-            newText = (TextView)findViewById(R.id.newTest);
-            String text = newText.getText().toString();
-
-            myRef.setValue(text);
         }
     }
 }
