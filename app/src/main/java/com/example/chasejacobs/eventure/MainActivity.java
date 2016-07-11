99ce2e5ff8354e3a127360d93bdb3c3f2bfa8296
Luke <luke.iannucci@gmail.com>
2016-07-11 15:32:33 -0600

    GPS test

--- app/src/main/java/com/example/chasejacobs/eventure/MainActivity.java
+++ app/src/main/java/com/example/chasejacobs/eventure/MainActivity.java
@@ -74,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
             createNetErrorDialog();
         }
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            int random = Build.VERSION.SDK_INT;
+            int random2 = Build.VERSION_CODES.M;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
             }
